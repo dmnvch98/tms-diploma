ee38c984c728508a2718dafc91d626a483815a89
dmnvch98 <dmnvch.evg@gmail.com>
2023-01-14 16:47:54 +0300

    init commit

---
+++ src/test/java/com/example/tmsdiploma/TmsDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmsdiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
