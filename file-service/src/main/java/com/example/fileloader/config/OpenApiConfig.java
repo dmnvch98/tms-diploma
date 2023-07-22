19589c146560d069137046498297bdcdaa3675d1
dmnvch98 <dmnvch.evg@gmail.com>
2023-07-22 19:01:03 +0300

    swagger was added to file service

---
+++ file-service/src/main/java/com/example/fileloader/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package com.example.fileloader.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "File service Rest API"
+        )
+)
+public class OpenApiConfig {
+    
+}
